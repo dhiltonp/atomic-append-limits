ff303c1a19f159eefdc144cc0df350b74ec91db9
David Hilton <david.hilton.p@gmail.com>
2021-11-18 14:39:23 -0700

    working stress test; 1 pass

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use std::io::{stdout, Write};
 use std::path::{Path, PathBuf};
-use std::{env, process, thread, time};
+use std::{env, fs, iter, process, thread, time};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -26,6 +29,17 @@ struct Cli {
     worker_id: u16,
 }
 
+fn random_chars(len: u32) -> String {
+    let mut rng = thread_rng();
+    let mut chars = iter::repeat(())
+        .map(|()| rng.sample(Alphanumeric))
+        .map(char::from)
+        .take(len as usize - 1)
+        .collect();
+    chars += "\n";
+    chars
+}
+
 fn spawn(id: u16, args: &Cli, process_path: &PathBuf) -> Result<process::Child, std::io::Error> {
     process::Command::new(&process_path)
         .arg("--worker-id")
@@ -33,7 +47,7 @@ fn spawn(id: u16, args: &Cli, process_path: &PathBuf) -> Result<process::Child, 
         .arg("--file")
         .arg(&args.file)
         .arg("--processes")
-        .arg("0")  // in case there is a bug, don't spawn more children :)
+        .arg("0") // in case there is a bug, don't spawn more children :)
         .arg("--times")
         .arg(format!("{}", args.times))
         .arg("--length")
@@ -41,30 +55,62 @@ fn spawn(id: u16, args: &Cli, process_path: &PathBuf) -> Result<process::Child, 
         .spawn()
 }
 
+fn parent_process(args: &Cli) {
+    let bytes = args.processes as u64 * args.times as u64 * args.length as u64 / 1024 / 1024;
+    println!(
+        "writing {}MB in {}B chunks across {} processes to {:?}",
+        bytes, args.length, args.processes, args.file
+    );
+    println!("truncating {:?}", &args.file);
+    fs::OpenOptions::new()
+        .truncate(true)
+        .write(true)
+        .open(&args.file);
+
+    println!("starting subprocesses");
+    let process_path = env::current_exe().unwrap();
+    let mut children = Vec::new();
+    for i in 1..args.processes + 1 {
+        if let Ok(child) = spawn(i, &args, &process_path) {
+            children.push(child);
+        } else {
+            println!("error starting child #{}", i);
+        }
+    }
+    thread::sleep(time::Duration::from_millis(250));
+    println!("\nwaiting for subprocesses");
+    for mut child in children {
+        child.wait();
+    }
+    println!("\npass completes");
+}
+
+fn child_process(args: &Cli) {
+    print!("{} ", args.worker_id);
+    stdout().flush();
+    let chars = random_chars(args.length);
+    let file = fs::OpenOptions::new()
+        .read(false)
+        .append(true)
+        .create(true)
+        .open(&args.file);
+    if let Ok(mut file) = file {
+        for i in 0..args.times {
+            let written = file.write(chars.as_bytes()).unwrap();
+            if written as u32 != args.length {
+                panic!("partial write: {}B written", written);
+            }
+        }
+    }
+    print!("{} ", args.worker_id);
+    stdout().flush();
+}
+
 fn main() {
     let args = Cli::from_args();
     if args.worker_id == 0 {
-        let bytes = args.processes as u32 * args.times * args.length / 1024 / 1024;
-        println!(
-            "writing {}MB in {}B chunks across {} processes to {:?}",
-            bytes, args.length, args.processes, args.file
-        );
-        println!("starting subprocesses");
-        let process_path = env::current_exe().unwrap();
-        let mut children = Vec::new();
-        for i in 1..args.processes + 1 {
-            if let Ok(child) = spawn(i, &args, &process_path) {
-                children.push(child);
-            } else {
-                println!("error starting child #{}", i);
-            }
-        }
-        thread::sleep(time::Duration::from_millis(250));
-        println!("\nwaiting for subprocesses");
-        for mut child in children {
-            child.wait();
-        }
+        parent_process(&args);
     } else {
-        print!("{} ", args.worker_id);
+        child_process(&args);
     }
 }
